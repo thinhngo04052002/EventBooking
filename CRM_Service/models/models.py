20b6100c42ae275de5957a4f424d7b2e118118f7
NAM LƯƠNG VĂN <20120531@student.hcmus.edu.vn>
2024-07-02 22:34:53 +0700

    CRM service

---
+++ CRM_Service/models/models.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel 
+from datetime import datetime
+from typing import List
+
+class Message_YC(BaseModel):
+    ThoiGian: datetime
+    NoiDung: str
+    NguoiGoi: int
+    
+class YC(BaseModel):
+    IDNguoiDung: int
+    TinNhan: List[Message_YC]
+
+class DanhGia(BaseModel):
+    ID: int
+    IDSukien: int
+    IDTaiKhoan: int
+    SoSao: int
+    phanhoi: str
+    IDDoiTac: int
+
+class PhanHoi(BaseModel):
+    ID: int
+    IDSukien: int
+    IDTaiKhoan: int
+    phanhoi: str 
+    IDDoiTac: int
+
+class sosao(BaseModel):
+    ID: int
+    IDSukien: int
+    IDTaiKhoan: int
+    SoSao: int
+    IDDoiTac: int
