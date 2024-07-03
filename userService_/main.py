d702d036436320ce4cbaa7fcd17f2eec29086884
THINH NGO <thinhngo04052002@gmail.com>
2024-07-03 14:35:31 +0700

    port user

--- userService_/main.py
+++ userService_/main.py
@@ -440,4 +440,4 @@ async def update_doitac_endpoint(user_id: int, update_data: NguoiDungUpdate, db:
     return update_nguoidung
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8006)  # Change the port here to 8006
+    uvicorn.run(app, port=8006)  # Change the port here to 8006
